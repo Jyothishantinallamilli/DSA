24445aae53c7b2e08395d793f020468bdeea883e
Jyothishantinallamilli <nallamillijyothishanti@gmail.com>
2024-06-01 18:42:00 +0530

    added

--- contains duplicates.java
+++ contains duplicates.java
@@ -1,20 +1,11 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        ArrayList<Integer> a=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            int count=0;
-            for(int j=0;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    count++;
+       Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+                if(nums[i]==nums[i+1]){
+                    return true;
                 }
-            }
-            a.add(count);
         }
-        for(int i=0;i<nums.length;i++){
-            if(a.get(i)>1){
-                return true;
-            }
-        }
-        return false;
+       return false;
     }
 }
